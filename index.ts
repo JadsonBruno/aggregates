018fd024a342dca243c19b6e5cc5a84bad21a07e
Jadson Bruno <34342808+JadsonBruno@users.noreply.github.com>
2020-11-02 09:59:28 -0300

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,15 @@
+/**
+ * IMPORTS
+ */
+import user from 'src/aggregates/user/reducer';
+import softphones from 'src/aggregates/softphones/reducer';
+
+
+/**
+ * EXPORTS
+ */
+export default
+{
+    user,
+    softphones
+};
