25fd0e6661b15f875037dadf04da17b557c521c1
Jadson Bruno <34342808+JadsonBruno@users.noreply.github.com>
2020-11-02 10:00:47 -0300

    Create execute.ts

---
+++ execute.ts
@@ -0,0 +1,39 @@
+/**
+ * IMPORTS
+ */
+import Store from 'src/store';
+
+
+/**
+ * TYPES
+ */
+import {IHandler} from 'src/aggregates/index.d';
+
+
+/**
+ * CODE
+ */
+
+/**
+ * I execute a command.
+ *
+ * :param command: command to be executed
+ * :param handler: handler that executes the given command
+ *
+ * :returns: promise with nothing
+ */
+async function execute<C> (command: C, handler: IHandler<C>): Promise<void>
+{
+    for await (const event of handler(command, Store.getState()))
+    {
+        Store.dispatch(event);
+    }
+}
+
+
+/**
+ * EXPORTS
+ */
+export {
+    execute
+};
